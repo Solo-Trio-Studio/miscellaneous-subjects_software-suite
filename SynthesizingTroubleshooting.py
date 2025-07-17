d5cd8ef81a6bebd65513399f8786046bf5fd1701
ietzel <67884277+ietzel@users.noreply.github.com>
2025-07-17 16:57:47 -0400

    Update SynthesizingTroubleshooting.py

--- SynthesizingTroubleshooting.py
+++ SynthesizingTroubleshooting.py
@@ -1,3 +1,3 @@
 
 def main():
-  print("Two is one, one is none. Sometimes these don't have to connect depending on energy scale")
+  print("Two is one, one is none. Depending on energy scale, you might be satisfied with some number of backups for critical systems.")
